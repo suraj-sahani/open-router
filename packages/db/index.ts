f513b567bcccca8eb9eccd74d41b25bc4067f515
suraj-sahani <suraj.sahani.961@gmail.com>
2026-03-08 21:03:17 +0530

    feat: Export db as package

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+})
+
+export const prisma = new PrismaClient({
+  adapter
+})
+
+
