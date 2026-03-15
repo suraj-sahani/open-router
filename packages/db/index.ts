f8685b2a1bf4f2ada868fa2c506e7c73839c94f2
suraj-sahani <suraj.sahani.961@gmail.com>
2026-03-15 23:36:41 +0530

    feat: Used pooling for db connection and direct connection for migrations

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,12 +1,10 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL
-})
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+});
 
 export const prisma = new PrismaClient({
-  adapter
-})
-
-
+  adapter,
+});
